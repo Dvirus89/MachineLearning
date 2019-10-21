98a60336b50ba4b89ca6f26661485d7fa133f3d2
Dvir Perets <Dvirus89@gmail.com>
2019-10-21 03:31:32 +0300

    Create FirstProject.py

---
+++ FirstProject.py
@@ -0,0 +1,40 @@
+## MY first machine learning project
+# Load libraries
+import pandas
+from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
+##
+#
+#
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+# Shuffle data - good for sorted data
+dataset = dataset.sample(frac=1)
+#making data_only for training
+data_only = dataset.drop('class',axis=1).values
+#
+#making classes_only for training
+classes_only = dataset['class'].values
+#
+#setting validation size and slicing what for training and what for testing
+validation_size = 0.8
+training_row_count = int(dataset.__len__() * validation_size)
+test_row_count = dataset.__len__() - training_row_count
+#
+data_learn = dataset.head(training_row_count).drop('class',axis=1).values
+classes_learn = dataset['class'].head(training_row_count).values
+#
+test_data = dataset.tail(test_row_count).drop('class',axis=1).values
+test_classes = dataset['class'].tail(test_row_count).values
+knn = KNeighborsClassifier()
+#train dataset ?
+knn.fit(data_learn, classes_learn)
+#
+# manual test of prediction after training
+for test,result in zip(test_data,test_classes):
+    test_list_item=[]
+    test_list_item.append(test)
+    predictions = knn.predict(test_list_item)
+    print("real result: " + result + " prediction: " + predictions)
